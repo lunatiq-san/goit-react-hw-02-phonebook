142349539501648580033aefc473ada0f533fdbd
Rozumiak Vladyslav <lunatiq86@gmail.com>
2021-07-19 15:22:10 +0300

    Add propTypes

--- src/components/ContactList/ContactList.jsx
+++ src/components/ContactList/ContactList.jsx
@@ -1,3 +1,5 @@
+import PropTypes from 'prop-types';
+
 const ContactList = ({ contacts, onDeleteContact }) => {
   return (
     <ul>
@@ -11,6 +13,17 @@ const ContactList = ({ contacts, onDeleteContact }) => {
       ))}
     </ul>
   );
+};
+
+ContactList.propTypes = {
+  contacts: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.string.isRequired,
+      name: PropTypes.string.isRequired,
+      number: PropTypes.number.isRequired,
+    }),
+  ).isRequired,
+  onDeleteContact: PropTypes.func.isRequired,
 };
 
 export default ContactList;
